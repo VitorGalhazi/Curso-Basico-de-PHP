0f91f2518807fe389a206be80c9b3aa942a4fecc
Vítor Miguel Galhazi <vmiguel970@gmail.com>
2022-07-25 21:45:00 -0300

    Atualizações

---
+++ indicespersonalizados.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+
+            $v = array (
+
+                1 = > "A",
+                9 = > "B",
+                3 = > "C",
+                6 = > "D",
+            );
+
+            $V[] = "E";
+            
+            
+        ?>
+    </body>
+</html>
