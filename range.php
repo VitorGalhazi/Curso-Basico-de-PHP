f7b6a6d36fa0e82e0939e39c4120bed2c6a2e684
Vítor Miguel Galhazi <vmiguel970@gmail.com>
2022-07-25 21:19:47 -0300

    Usando o range.
    Aprendemos a utilizar a estrutura do range em PHP.

---
+++ range.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php
+
+            $n = range(5, 20, 5);
+            print_r($n);
+            
+        ?>
+    </body>
+</html>
